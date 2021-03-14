b6de73b04b005714a652c7ddc9b76d2236c70c19
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-03-14 20:19:58 +0600

    interface created for owner, pet, vet

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/mahim/petclinic/service/OwnerService.java
@@ -0,0 +1,14 @@
+package com.mahim.petclinic.service;
+
+import com.mahim.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
