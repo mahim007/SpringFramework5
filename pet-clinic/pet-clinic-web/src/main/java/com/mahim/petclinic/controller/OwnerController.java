40d2e5c5c8a44ccf78f9dafb0d471454c9cdc21b
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-03-20 16:56:44 +0600

    List all owners on owners index page. Closes #6

--- pet-clinic/pet-clinic-web/src/main/java/com/mahim/petclinic/controller/OwnerController.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/mahim/petclinic/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package com.mahim.petclinic.controller;
 
+import com.mahim.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/" , "/index", "/index.html"})
-    public String ownerList() {
+    public String ownerList(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
