10cb79e80c5c0a6a32be8758c4f6e3bfd5ecd9b5
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-04-03 19:06:46 +0600

    multiple property files used

---
+++ jokes-app/src/main/java/com/mahim/jokesapp/FakeJmsBroker.java
@@ -0,0 +1,40 @@
+package com.mahim.jokesapp;
+
+public class FakeJmsBroker {
+    private String username;
+    private String password;
+    private String url;
+
+    public FakeJmsBroker() {
+    }
+
+    public FakeJmsBroker(String username, String password, String url) {
+        this.username = username;
+        this.password = password;
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
