6049bcfa0f463262b74ec6e999e5e2620f386e1e
mahim <ashrafulmahim@gmail.com>
2021-05-19 21:25:02 +0600

    data.sql created, query method added in repo

--- recipe-app/src/main/java/com/mahim/recipeapp/controllers/IndexController.java
+++ recipe-app/src/main/java/com/mahim/recipeapp/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package com.mahim.recipeapp.controllers;
 
+import com.mahim.recipeapp.domain.Category;
+import com.mahim.recipeapp.domain.UnitOfMeasure;
+import com.mahim.recipeapp.repositories.CategoryRepository;
+import com.mahim.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("category: " + categoryOptional.get().getId() + " " + categoryOptional.get().getDescription());
+        System.out.println("unit of measure: " + unitOfMeasureOptional.get().getId() + " " + unitOfMeasureOptional.get().getDescription());
         return "index";
     }
 }
--- recipe-app/src/main/java/com/mahim/recipeapp/repositories/UnitOfMeasureRepository.java
+++ recipe-app/src/main/java/com/mahim/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.mahim.recipeapp.repositories;
 import com.mahim.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
