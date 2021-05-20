7757e82d5b3580459719b8e4793337ead2505122
mahim <ashrafulmahim@gmail.com>
2021-05-20 10:49:06 +0600

    setter method updated for setNotes

--- recipe-app/src/main/java/com/mahim/recipeapp/domain/Recipe.java
+++ recipe-app/src/main/java/com/mahim/recipeapp/domain/Recipe.java
@@ -126,6 +126,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
