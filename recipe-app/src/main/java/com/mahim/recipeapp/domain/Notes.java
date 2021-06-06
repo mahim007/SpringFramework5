580eba8db7d797cca54e256ac781a6fab4e36505
Ashraful Islam Mahim <ashrafulmahim@gmailcom>
2021-06-06 22:57:25 +0600

    project lombok added

--- recipe-app/src/main/java/com/mahim/recipeapp/domain/Notes.java
+++ recipe-app/src/main/java/com/mahim/recipeapp/domain/Notes.java
@@ -1,7 +1,10 @@
 package com.mahim.recipeapp.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Notes {
     @Id
@@ -17,27 +20,8 @@ public class Notes {
     public Notes() {
     }
 
-    public Long getId() {
-        return id;
+    protected boolean canEqual(final Object other) {
+        return other instanceof Notes;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
