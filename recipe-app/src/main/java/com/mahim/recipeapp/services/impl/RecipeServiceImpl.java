2587fc31af77eb1a7848d0fca71b8132ffdc692a
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-06-12 19:35:43 +0600

    lombok used for domain object and service

--- recipe-app/src/main/java/com/mahim/recipeapp/services/impl/RecipeServiceImpl.java
+++ recipe-app/src/main/java/com/mahim/recipeapp/services/impl/RecipeServiceImpl.java
@@ -3,11 +3,13 @@ package com.mahim.recipeapp.services.impl;
 import com.mahim.recipeapp.domain.Recipe;
 import com.mahim.recipeapp.repositories.RecipeRepository;
 import com.mahim.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am in the recipe service");
+        
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
