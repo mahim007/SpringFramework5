97941e09060fb436ad415186ff96820153900365
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2021-06-12 22:41:03 +0600

    @Data is used in Recipe model
    a circular referencing was happening due bi-directional jpa annotation.
    
    @EqualsAndHashCode(exclude = {"recipes"})
    is used to prevent this error.
    everything works as expected now.

--- recipe-app/src/main/java/com/mahim/recipeapp/domain/Recipe.java
+++ recipe-app/src/main/java/com/mahim/recipeapp/domain/Recipe.java
@@ -1,15 +1,12 @@
 package com.mahim.recipeapp.domain;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
 @Entity
 public class Recipe {
     @Id
