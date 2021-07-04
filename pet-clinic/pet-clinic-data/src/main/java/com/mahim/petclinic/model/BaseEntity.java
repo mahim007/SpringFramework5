935e5d9a94939b622cf446c9583859a153300b64
mahim007 <ashrafulmahim@gmail.com>
2021-07-04 21:31:50 +0600

    random lines removed

--- pet-clinic/pet-clinic-data/src/main/java/com/mahim/petclinic/model/BaseEntity.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/mahim/petclinic/model/BaseEntity.java
@@ -18,15 +18,4 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private Integer data;
-    private Boolean isUpdated;
-
-    private String doSomething() {
-        if (StringUtils.containsWhitespace(name) || StringUtils.hasText(name)) {
-            System.out.println(name);
-        }
-
-        return name.concat(" welcome to the java world");
-    }
 }
