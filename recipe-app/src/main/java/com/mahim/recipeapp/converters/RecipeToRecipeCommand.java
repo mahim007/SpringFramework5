caf7d3b4b013deda7f7a1fff4552d74ab78da19a
mahim007 <ashrafulmahim@gmail.com>
2021-07-24 16:35:21 +0600

    image not showing (backend bug) issue fixed

--- recipe-app/src/main/java/com/mahim/recipeapp/converters/RecipeToRecipeCommand.java
+++ recipe-app/src/main/java/com/mahim/recipeapp/converters/RecipeToRecipeCommand.java
@@ -37,7 +37,7 @@ public class RecipeToRecipeCommand implements Converter<Recipe, RecipeCommand> {
         recipeCommand.setSource(recipe.getSource());
         recipeCommand.setUrl(recipe.getUrl());
         recipeCommand.setNotes(notesConverter.convert(recipe.getNotes()));
-        recipeCommand.setImage(recipeCommand.getImage());
+        recipeCommand.setImage(recipe.getImage());
 
         if (recipe.getCategories() != null && recipe.getCategories().size() > 0) {
             recipe.getCategories()
